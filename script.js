d2fa7622ebaf2d66408479f9ffcbd05cb61c00a5
Lucas Alexsander <lucas10barbosa_@hotmail.com>
2023-02-23 12:46:26 -0300

    Second commit

--- script.js
+++ script.js
@@ -8,37 +8,42 @@ const timezoneCamp = document.querySelector("#timezone");
 const ispCamp = document.querySelector("#isp");
 
 // Mapa
-    var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView([51.505, -0.09], 13);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('Your IP is from here!')
-    .openPopup();
+var latlng = L.latLng(34.04915, -118.09462);
+map.panTo(latlng);
+
+L.marker([34.04915, -118.09462])
+  .addTo(map)
+  .bindPopup("Your IP is from here!")
+  .openPopup();
 
 const tratamento = (e) => {
-
-    console.log(e);
+  console.log(e);
 
   const { ip, location, isp } = e;
-
 
   ipCamp.innerHTML = ip;
   locationCamp.innerHTML = location.city;
   timezoneCamp.innerHTML = "UTC " + location.timezone;
   ispCamp.innerHTML = isp;
 
+  console.log(location);
+
   // Mapa
   var latlng = L.latLng(location.lat, location.lng);
-  map.panTo(latlng)
+  map.panTo(latlng);
 
-  L.marker([location.lat, location.lng]).addTo(map)
-  .bindPopup('Your IP is from here!')
-  .openPopup();
+  L.marker([location.lat, location.lng])
+    .addTo(map)
+    .bindPopup("Your IP is from here!")
+    .openPopup();
 };
 
 const transformarEmJson = (response) => {
